c50fd16b667aaf10e9840b15db68bbad960bff47
RodrigoJScript <rodrigo777.rjp@gmail.com>
2022-08-17 17:50:04 -0500

    Mensaje de campo vacio

--- app/src/main/java/com/rodrigojscript/gu_ia/ui/screens/MainScreen.kt
+++ app/src/main/java/com/rodrigojscript/gu_ia/ui/screens/MainScreen.kt
@@ -1,80 +1,85 @@
 package com.rodrigojscript.gu_ia.ui.screens
 
-import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.layout.*
+import android.content.Context
+import android.widget.Toast
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 import com.rodrigojscript.gu_ia.ui.components.CustomTextField
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
 
 var totalTodo: Double = 0.0
 
 @Composable
 fun MainScreen() {
-    var nmb by remember { mutableStateOf("") }
-    var cod by remember { mutableStateOf("") }
-    var pre by remember { mutableStateOf("") }
-    var can by remember { mutableStateOf("") }
-    var nmb1 by remember { mutableStateOf("") }
-    var cod1 by remember { mutableStateOf("") }
-    var pre1 by remember { mutableStateOf("") }
-    var can1 by remember { mutableStateOf("") }
-    var nmb2 by remember { mutableStateOf("") }
-    var cod2 by remember { mutableStateOf("") }
-    var pre2 by remember { mutableStateOf("") }
-    var can2 by remember { mutableStateOf("") }
-    var nmb3 by remember { mutableStateOf("") }
-    var cod3 by remember { mutableStateOf("") }
-    var pre3 by remember { mutableStateOf("") }
-    var can3 by remember { mutableStateOf("") }
-    var nmb4 by remember { mutableStateOf("") }
-    var cod4 by remember { mutableStateOf("") }
-    var pre4 by remember { mutableStateOf("") }
-    var can4 by remember { mutableStateOf("") }
-    var nmb5 by remember { mutableStateOf("") }
-    var cod5 by remember { mutableStateOf("") }
-    var pre5 by remember { mutableStateOf("") }
-    var can5 by remember { mutableStateOf("") }
-    var nmb6 by remember { mutableStateOf("") }
-    var cod6 by remember { mutableStateOf("") }
-    var pre6 by remember { mutableStateOf("") }
-    var can6 by remember { mutableStateOf("") }
-    var nmb7 by remember { mutableStateOf("") }
-    var cod7 by remember { mutableStateOf("") }
-    var pre7 by remember { mutableStateOf("") }
-    var can7 by remember { mutableStateOf("") }
-    var nmb8 by remember { mutableStateOf("") }
-    var cod8 by remember { mutableStateOf("") }
-    var pre8 by remember { mutableStateOf("") }
-    var can8 by remember { mutableStateOf("") }
-    var nmb9 by remember { mutableStateOf("") }
-    var cod9 by remember { mutableStateOf("") }
-    var pre9 by remember { mutableStateOf("") }
-    var can9 by remember { mutableStateOf("") }
+    val scaffoldState: ScaffoldState = rememberScaffoldState()
+    val coroutineScope: CoroutineScope = rememberCoroutineScope()
+    var nmb by rememberSaveable { mutableStateOf("") }
+    var cod by rememberSaveable { mutableStateOf("") }
+    var pre by rememberSaveable { mutableStateOf("") }
+    var can by rememberSaveable { mutableStateOf("") }
+    var nmb1 by rememberSaveable { mutableStateOf("") }
+    var cod1 by rememberSaveable { mutableStateOf("") }
+    var pre1 by rememberSaveable { mutableStateOf("") }
+    var can1 by rememberSaveable { mutableStateOf("") }
+    var nmb2 by rememberSaveable { mutableStateOf("") }
+    var cod2 by rememberSaveable { mutableStateOf("") }
+    var pre2 by rememberSaveable { mutableStateOf("") }
+    var can2 by rememberSaveable { mutableStateOf("") }
+    var nmb3 by rememberSaveable { mutableStateOf("") }
+    var cod3 by rememberSaveable { mutableStateOf("") }
+    var pre3 by rememberSaveable { mutableStateOf("") }
+    var can3 by rememberSaveable { mutableStateOf("") }
+    var nmb4 by rememberSaveable { mutableStateOf("") }
+    var cod4 by rememberSaveable { mutableStateOf("") }
+    var pre4 by rememberSaveable { mutableStateOf("") }
+    var can4 by rememberSaveable { mutableStateOf("") }
+    var nmb5 by rememberSaveable { mutableStateOf("") }
+    var cod5 by rememberSaveable { mutableStateOf("") }
+    var pre5 by rememberSaveable { mutableStateOf("") }
+    var can5 by rememberSaveable { mutableStateOf("") }
+    var nmb6 by rememberSaveable { mutableStateOf("") }
+    var cod6 by rememberSaveable { mutableStateOf("") }
+    var pre6 by rememberSaveable { mutableStateOf("") }
+    var can6 by rememberSaveable { mutableStateOf("") }
+    var nmb7 by rememberSaveable { mutableStateOf("") }
+    var cod7 by rememberSaveable { mutableStateOf("") }
+    var pre7 by rememberSaveable { mutableStateOf("") }
+    var can7 by rememberSaveable { mutableStateOf("") }
+    var nmb8 by rememberSaveable { mutableStateOf("") }
+    var cod8 by rememberSaveable { mutableStateOf("") }
+    var pre8 by rememberSaveable { mutableStateOf("") }
+    var can8 by rememberSaveable { mutableStateOf("") }
+    var nmb9 by rememberSaveable { mutableStateOf("") }
+    var cod9 by rememberSaveable { mutableStateOf("") }
+    var pre9 by rememberSaveable { mutableStateOf("") }
+    var can9 by rememberSaveable { mutableStateOf("") }
 
     var valTicket by remember { mutableStateOf("") }
-    var tot by remember { mutableStateOf(0.0) }
-    var tot1 by remember { mutableStateOf(0.0) }
-    var tot2 by remember { mutableStateOf(0.0) }
-    var tot3 by remember { mutableStateOf(0.0) }
-    var tot4 by remember { mutableStateOf(0.0) }
-    var tot5 by remember { mutableStateOf(0.0) }
-    var tot6 by remember { mutableStateOf(0.0) }
-    var tot7 by remember { mutableStateOf(0.0) }
-    var tot8 by remember { mutableStateOf(0.0) }
-    var tot9 by remember { mutableStateOf(0.0) }
+    var tot: Double? by remember { mutableStateOf(0.0) }
+    var tot1: Double? by remember { mutableStateOf(0.0) }
+    var tot2: Double? by remember { mutableStateOf(0.0) }
+    var tot3: Double? by remember { mutableStateOf(0.0) }
+    var tot4: Double? by remember { mutableStateOf(0.0) }
+    var tot5: Double? by remember { mutableStateOf(0.0) }
+    var tot6: Double? by remember { mutableStateOf(0.0) }
+    var tot7: Double? by remember { mutableStateOf(0.0) }
+    var tot8: Double? by remember { mutableStateOf(0.0) }
+    var tot9: Double? by remember { mutableStateOf(0.0) }
+    var valTicketN: Double? by remember { mutableStateOf(0.0) }
     var totalTod by remember { mutableStateOf(totalTodo.toString()) }
-    var expanded by remember { mutableStateOf(false) }
-    var stateLay = rememberLazyListState()
-    Scaffold(topBar = { TopAppBar(title = { Text(text = "Ticket") }) }) {
+    val stateLay = rememberLazyListState()
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = { TopAppBar(title = { Text(text = "Ticket") }) }) {
         LazyColumn(modifier = Modifier.fillMaxSize(), state = stateLay) {
             item {
                 Column {
@@ -346,18 +351,27 @@ fun MainScreen() {
 
                     Text(text = totalTod)
                     Button(onClick = {
-                        tot = can.toDouble() * pre.toDouble()
-                        tot1 = can1.toDouble() * pre1.toDouble()
-                        tot2 = can2.toDouble() * pre2.toDouble()
-                        tot3 = can3.toDouble() * pre3.toDouble()
-                        tot4 = can4.toDouble() * pre4.toDouble()
-                        tot5 = can5.toDouble() * pre5.toDouble()
-                        tot6 = can6.toDouble() * pre6.toDouble()
-                        tot7 = can7.toDouble() * pre7.toDouble()
-                        tot8 = can8.toDouble() * pre8.toDouble()
-                        tot9 = can9.toDouble() * pre9.toDouble()
+                        if (valTicket.isEmpty()) {
+                            coroutineScope.launch {
+                                scaffoldState.snackbarHostState.showSnackbar(
+                                    message = "Campo ticket esta en blanco",
+                                    duration = SnackbarDuration.Short
+                                )
+                            }
+                        }
+                        tot = isNull(can, pre)
+                        tot1 = isNull(can1, pre1)
+                        tot2 = isNull(can2, pre2)
+                        tot3 = isNull(can3, pre3)
+                        tot4 = isNull(can4, pre4)
+                        tot5 = isNull(can5, pre5)
+                        tot6 = isNull(can6, pre6)
+                        tot7 = isNull(can7, pre7)
+                        tot8 = isNull(can8, pre8)
+                        tot9 = isNull(can9, pre9)
+                        valTicketN = isNullT(valTicket)
                         totalTod =
-                            (tot + tot1 + tot2 + tot3 + tot4 + tot5 + tot6 + tot7 + tot8 + tot9 + valTicket.toDouble()).toString()
+                            (tot!! + tot1!! + tot2!! + tot3!! + tot4!! + tot5!! + tot6!! + tot7!! + tot8!! + tot9!! + valTicketN!!).toString()
                     }) {
                         Text(text = "Calcular")
                     }
@@ -367,3 +381,18 @@ fun MainScreen() {
     }
 }
 
+fun isNull(can: String, pre: String): Double? {
+    return try {
+        can.toDouble() * pre.toDouble()
+    } catch (e: NumberFormatException) {
+        0.0
+    }
+}
+
+fun isNullT(input: String): Double? {
+    return try {
+        input.toDouble()
+    } catch (e: NumberFormatException) {
+        0.0
+    }
+}
